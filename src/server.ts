ad798f2d660283a5bf121ec9981f257c97054acc
jtiagosantos42 <jtiagosantos@brq.com>
2026-03-26 14:35:03 -0400

    add route for generate a random id

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import { generateId } from "./helpers/generate-id.js";
 
 const app = express();
 const PORT = 3000;
 
 app.get("/hello", (req, res) => {
   res.json({ ok: true });
+});
+
+app.get("/id", (req, res) => {
+  const uuid = generateId();
+  res.json({ id: uuid });
 });
 
 app.listen(PORT, () => {
